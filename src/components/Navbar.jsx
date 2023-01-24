ce0b731e350adf046c62ef630476c7ff1359801d
Muhammad Danish Azeem <Muhammaddanish1458@gmail.com>
2023-01-24 10:41:52 +0500

    Updates like time author badge

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,12 +18,12 @@ export default class Navbar extends Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/"><img src="https://img.icons8.com/3d-fluency/94/null/pet-commands-follow.png" alt="" />NewsCat</Link>
+          <Link className="navbar-brand" to="/" onClick={()=>document.getElementById("first").click()}><img src="https://img.icons8.com/3d-fluency/94/null/pet-commands-follow.png" alt="" />NewsCat</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse justify-content-around" id="navbarNav">
-            <Link className="nav-link colo activ" onClick={this.actEvent} to="/">Top Headlines 🔴</Link>
+            <Link className="nav-link colo activ" onClick={this.actEvent} to="/" id="first">Top Headlines 🔴</Link>
             <Link className="nav-link colo" onClick={this.actEvent} to="/business">Business 📈</Link>
             <Link className="nav-link colo" onClick={this.actEvent} to="/entertainment">Entertainment 🎪</Link>
             <Link className="nav-link colo" onClick={this.actEvent} to="/health">Health 💊</Link>
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -3,14 +3,17 @@ import './card.css'
 
 export class NewsItem extends Component {
     render() {
-        let {title, url, description, publishedAt, urlToImage} = this.props.article;
+        let {title, url, description, publishedAt, urlToImage, author, source} = this.props.article;
     return (
     <div className="card my-3">
         <img src={urlToImage} className="card-img-top" alt=""/>
         <div className="card-body">
             <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <p className="card-text"><small className="text-muted">Updated: {publishedAt}</small></p>
+            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-info">
+                {source.name}
+            </span>
+            <p className="card-text">{!description?"No Description":description.slice(0,200)}</p>
+            <p className="card-text"><small className="text-muted">By {author||"Unknown"} Updated at {new Date(publishedAt).toGMTString()}</small></p>
             <a id="url" href={url} rel="noreferrer" target="_blank" className="btn btn-sm btn-dark">Read More...</a>
         </div>
     </div>
